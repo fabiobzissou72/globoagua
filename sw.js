8d7a6d328bf9a41a4ef8a0445f64fdf79ec76710
Fabio Zissou <fabiobz@gmail.com>
2026-02-25 12:26:09 -0300

    feat: menu de clientes no admin e melhoria na ativação do PWA

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // GLOBO ÁGUA - Service Worker
 // ============================================================
-const CACHE_NAME = 'globo-agua-v9.5';
+const CACHE_NAME = 'globo-agua-v9.6';
 
 const STATIC_ASSETS = [
   './',
