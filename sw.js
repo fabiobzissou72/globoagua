919ef4f6f524a4c09737e6f1b0ce59b169ea8bf2
Fabio Zissou <fabiobz@gmail.com>
2026-02-24 22:11:58 -0300

    Correções de UI, Autenticação, Cache e Manutenção Geral

--- sw.js
+++ sw.js
@@ -28,8 +28,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        // Removido o filtro: Limpa ABSOLUTAMENTE TUDO ao ativar para garantir frescor total
-        keys.map(k => caches.delete(k))
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
       )
     )
   );
